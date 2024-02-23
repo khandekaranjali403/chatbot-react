30cd40f0bab2d7f3308171031ddc7332dd2daa08
Anjali Khandekar <khandekaranjali403@gmail.com>
2024-02-23 17:08:55 +0530

    Chatbot-2024

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import { Notifications } from 'react-push-notification';
+import NotificationComponent from './module/NotificationComponent';
+import { SimpleChatBox } from './module/SimpleChatBox';
+import SimpleForm from './module/form/SimpleForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Notifications /> */}
+      {/*  */}
+      {/* <SimpleChatBox /> */}
+      <SimpleForm />
     </div>
   );
 }
